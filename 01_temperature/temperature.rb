064329793201926eb798c2c5e62dcfb6f81f8c4e
Dennis Christman <dennis.christman@gmail.com>
2017-02-17 07:57:23 -0500

    Completed exercies 00 - 02

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc temperature
+  temperature = (temperature.to_f - 32) * 5/9
+end
+
+def ctof temperature
+  temperature = temperature.to_f * 9/5 + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,34 @@
 #write your code here
+def add num1, num2
+  num1 + num2
+end
+def subtract num1, num2
+  num1 - num2
+end
+def sum num_array
+  sum = 0
+  num_array.each do |num|
+    sum = sum + num
+  end
+  sum
+end
+
+def multiply num_array
+  answer = 1
+  num_array.each do |num|
+    answer = answer * num
+  end
+  answer
+end
+
+def power num, exp
+  num ** exp
+end
+def factorial num
+  answer = 1
+  while num > 1
+    answer = answer * num
+    num = num - 1
+  end
+  answer
+end
