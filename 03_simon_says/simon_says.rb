bdfcab030e1c4c834cb4157e11302688f137baad
Dennis Christman <dennis.christman@gmail.com>
2017-02-17 10:39:12 -0500

    Finished problems 03 and 04

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,39 @@
-#write your code here
+IGNORES = %w( the of a and over )
+
+def echo msg
+  msg
+end
+
+def shout msg
+  msg.upcase
+end
+
+def repeat msg, times=2
+  msg = msg + ' '
+  (msg * times).strip
+end
+
+def start_of_word msg, chars
+  msg.slice(0,chars)
+end
+
+def first_word msg
+  msg = msg.split
+  msg[0]
+end
+
+def titleize msg
+
+
+  msg = msg.capitalize
+  msg = msg.split
+  msg.map! do |toCapitalize|
+    if (IGNORES.include?(toCapitalize) == false)
+      toCapitalize.capitalize
+    else
+      toCapitalize
+    end
+  end
+  msg.join (' ')
+
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,26 @@
 #write your code here
+VOWELS = [ 'a', 'e', 'i', 'o', 'u' ]
+
+def translate msg
+  msg = msg.split
+  msg.map! do |word|
+    if (VOWELS.include?(word.slice(0,1)))
+      word = word + 'ay'
+    else
+      word = word.split(//)
+      while (VOWELS.include?(word[0]) == false)
+
+        word << word[0]
+        if (word[0]=='q')
+          word.delete_at(0)
+          word << word[0]
+        end
+        word.delete_at(0)
+      end
+      word = word.join
+      word = word + 'ay'
+    end
+
+  end
+  msg = msg.join(' ')
+end
