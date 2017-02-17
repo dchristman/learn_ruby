86673f6be544cef75d8b6050626b137702cd66b9
Dennis Christman <dennis.christman@gmail.com>
2017-02-17 11:19:20 -0500

    Finished all assigned exercises (00-05)

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,97 @@
 class Book
-# write your code here
+  ##List of words to ignore: articles, conjunctions, and prepositions(taken from a list online)
+  IGNORES = %w( the of a an and or nor
+aboard
+about
+above
+across
+after
+against
+along
+amid
+among
+anti
+around
+as
+at
+before
+behind
+below
+beneath
+beside
+besides
+between
+beyond
+but
+by
+concerning
+considering
+despite
+down
+during
+except
+excepting
+excluding
+following
+for
+from
+in
+inside
+into
+like
+minus
+near
+of
+off
+on
+onto
+opposite
+outside
+over
+past
+per
+plus
+regarding
+round
+save
+since
+than
+through
+to
+toward
+towards
+under
+underneath
+unlike
+until
+up
+upon
+versus
+via
+with
+within
+without)
+
+  def initialize
+    @title = ''
+  end
+  def title=(title)
+    @title = titleize(title)
+  end
+  def title
+    @title
+  end
+
+  def titleize msg
+    msg = msg.capitalize
+    msg = msg.split
+    msg.map! do |toCapitalize|
+      if (IGNORES.include?(toCapitalize) == false)
+        toCapitalize.capitalize
+      else
+        toCapitalize
+      end
+    end
+    msg.join (' ')
+  end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,38 @@
 class Timer
   #write your code here
+  def initialize
+    @seconds = 0
+    @mins = 0
+    @hours = 0
+  end
+  def seconds
+    @seconds
+  end
+  def seconds= (seconds)
+    @seconds = seconds
+    if @seconds > 60
+      @mins = @seconds / 60
+      @seconds = @seconds % 60
+      if @mins > 60
+        @hours = @mins / 60
+        @mins = @mins % 60
+      end
+    end
+  end
+  def time_string
+    str = ''
+    if @hours.to_s.length == 1
+      str = str + '0'
+    end
+    str = str + @hours.to_s + ':'
+    if @mins.to_s.length == 1
+      str = str + '0'
+    end
+    str = str + @mins.to_s + ':'
+    if @seconds.to_s.length == 1
+      str = str + '0'
+    end
+    str = str + @seconds.to_s
+  end
+
 end
